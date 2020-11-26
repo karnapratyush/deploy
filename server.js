0d3a4989e31e575a5847c859e0ef48fe60031916
karnapratyush <pratyushkumarkarna@gmail.com>
2020-11-26 14:38:42 +0530

    fun2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express()
 const PORT=process.env.PORT||4444
 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.send('hello world. how are you?')
 })
 
 app.listen(PORT,(req,res)=>{
